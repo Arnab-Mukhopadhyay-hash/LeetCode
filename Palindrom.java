1a74650f17036096026e1df0d79c790b4180969a
Arnab-Mukhopadhyay-hash <84343247+Arnab-Mukhopadhyay-hash@users.noreply.github.com>
2022-08-27 11:23:45 +0530

    Comments Added

--- Palindrom.java
+++ Palindrom.java
@@ -4,6 +4,8 @@ import java.util.Scanner;
 public class Palindrom {
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
+		
+		/* A rather brute force approach with integers the complexity is O(lenght of the number) */
 //		int n = scan.nextInt();
 //		int dn = n;
 //		int sum = 0;
@@ -16,6 +18,7 @@ public class Palindrom {
 //		else System.out.println("false");
 		
 		/* Using strings */
+		// A simpler approach
 		String str = scan.next();
 		String nstr = "";
 		for(int i = 0; i <str.length(); i++) {
